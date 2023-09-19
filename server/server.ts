df9f1fd103b0dae4371cc54b967455df81baf263
ishaksebsib <ishaksebsib@gmail.com>
2023-09-19 13:08:25 +0300

    Conveted to typescript

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 //
 const app = express();
@@ -11,14 +11,14 @@ const port = 8000;
 const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(process.cwd() + "/index.html");
 });
 
 io.on("connection", (socket) => {
   console.log("a user connected");
 
   // listen if chat message is appiers
-  socket.on("chat message", (msg) => {
+  socket.on("chat message", (msg: string) => {
     console.log("message: " + msg); // log the message we recived from users
     io.emit("chat message", msg); // send back / Brodcast the message for all connected users
   });
