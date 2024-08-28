a445fd502a8d531befd0f3db984695b00cc6ebae
ishak <ishaksebsib@gmail.com>
2024-08-28 15:06:00 +0300

    [ENH] basic hello world server

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("Starting server started on localhost:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+
+}
