52921ef492971cfed93dbeb4407d67d17202662a
ishak <ishaksebsib@gmail.com>
2024-08-28 16:39:44 +0300

    [ENH] basic path with path validation Added

--- main.go
+++ main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	pathValue := r.PathValue("id")
+	id, err := strconv.Atoi(pathValue)
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("GET /", home)
+	mux.HandleFunc("GET /snippet/{id}", snippetView)
+	mux.HandleFunc("POST /snippet/create", snippetCreate)
 
 	log.Print("Starting server started on localhost:4000")
 	err := http.ListenAndServe(":4000", mux)
